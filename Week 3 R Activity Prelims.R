1f9ea98225837c917e49f18ee02fa5af16e7f3c6
Michael Eniolade <89307219+MichaelEnny@users.noreply.github.com>
2021-11-05 15:41:33 -0700

    Update Week 3 R Activity Prelims.R

--- Week 3 R Activity Prelims.R
+++ Week 3 R Activity Prelims.R
@@ -86,3 +86,5 @@ ggplot(NewWomen, aes(x=height, y=weight)) +
 #Optional 'Just for fun' TASK: Change the arguments for the geom point and see what happens to the plot
 ggplot(NewWomen, aes(x=height, y=weight)) + 
   geom_point(shape = 21, colour = "green", fill = "red", size = 5, stroke = 5)
+  
+  
